3dfeb43491e04fd60f4f227971afc7e50d63729b
yuntaewon <sun10000ss@gmail.com>
2026-03-30 14:47:13 +0900

    문자 배열 실습

---
+++ array/-5.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void) {
+	char str[30] = "C langure";
+	int i = 0;
+	
+	while (str[i] != "\0")
+		i++;
+	printf("문자열\%s\"의 길이는 %d입니다.\n", str, i);
+
+	int a;
+	char menu[5][10] = {
+		"init",
+		"open",
+		"close",
+		"read",
+		"write"
+	};
+
+	for (a = 0;a < 5;a++)
+		printf("%d 번쨰 메뉴:%s\n", a, menu);
+
+	return 0;
+}
