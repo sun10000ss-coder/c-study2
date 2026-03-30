ac9820d904695cd0a647fd41dff917c83e4272c3
yuntaewon <sun10000ss@gmail.com>
2026-03-30 15:18:21 +0900

    c언어

---
+++ array/char-array-1.c
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+    char str[30] = "C language";//문자배열 최대 30자
+    int i = 0;//0부터 시작
+
+    while (str[i] != '\0')//만족할때 반복
+        i++;              // I= 다르다 \0는 NULL 값을 의미
+                          //문자열 내용 끝나는 시점 반드시 NULL이 들어감  
+    printf("문자열\"%s\"의 길이는 %d입니다.\n", str, i);
+    return 0;
+}
---
+++ array/char-arry-2.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   
+#include <stdio.h>
+int main(void)
+{
+    int i;
+    char fruits[3][20];/*５행　１０열（０～４행　０～９열）*/
+
+
+    for (i = 1; i <= 3; i++)//0~2
+    {
+        printf("과일 이름을 입력하시요:");
+        scanf("%s", fruits[i]);
+        printf("%d 번째 과일은:%s\n", i, fruits[i]); //menu[i]는 행을 의미
+    }
+   
+
+    return 0;
+}
